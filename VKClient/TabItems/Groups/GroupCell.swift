8377db06474a7eabd9b4cf2a7dd0fb104d7bb512
anttOonI <93sant@mail.ru>
2020-11-01 23:01:10 +0300

    HW Lesson 2

---
+++ VKClient/TabItems/Groups/GroupCell.swift
@@ -0,0 +1,23 @@
+//
+//  GroupCell.swift
+//  VKClient
+//
+//  Created by AntonSobolev on 01.11.2020.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
